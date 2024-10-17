1303abed0130676d449d58652d4e70737f6c1e4c
UgandanNut <cruchyn@gmail.com>
2024-10-17 16:11:33 +0700

    feat: slicing privacy data dan nambah route utk blog

--- resources/js/Pages/Privasidata.vue
+++ resources/js/Pages/Privasidata.vue
@@ -13,19 +13,19 @@ import ApplicationLogo from '@/Components/ApplicationLogo.vue';
     <div class="flex gap-10 px-[250px] bg-white items-center text-center">
     
     <AuthenticationCard class="w-1/2 text-center">
-<div class="mb-8 text-primary">
-            <h1>Keamanan & Privasi Data</h1>
-            <h5>Sistem tersertifikasi standar regulasi dan ketentuan keamanan data.</h5>
+<div class="mb-8 text-primary ">
+            <h1 class="mb-[5%]">Keamanan & Privasi Data</h1>
+            <h5 class="mb-[5%]">Sistem tersertifikasi standar regulasi dan ketentuan keamanan data.</h5>
             <h6>Privasi Anda adalah prioritas kami. Dengan teknologi mutakhir, data Anda tetap aman dan hanya digunakan sesuai hukum yang berlaku.</h6>
         </div>
      </AuthenticationCard>
-    <div class="w-1/2  object-cover object-left">
-    <h5 class="text-primary">●Enkripsi Data & File</h5>
-    <h5 class="text-primary">●Otentikasi Ganda (2FA)</h5>
-    <h5 class="text-primary">●Pemulihan Data (backup)</h5>
-    <h5 class="text-primary">●Session Management</h5>
-    <h5 class="text-primary">●Konfirmasi Password Dalam Aksi Penting</h5>
-    <h5 class="text-primary">●Verifikasi Email</h5>
+    <div class="w-1/2  object-cover object-left  h-50 grid grid-cols-3 gap-10">
+    <h5 class="text-secondary">●Enkripsi Data & File</h5>
+    <h5 class="text-secondary">●Otentikasi Ganda (2FA)</h5>
+    <h5 class="text-secondary">●Pemulihan Data </h5>
+    <h5 class="text-secondary">●Session Management</h5>
+    <h5 class="text-secondary">●Konfirmasi Password Dalam Aksi Penting</h5>
+    <h5 class="text-secondary">●Verifikasi Email</h5>
     </div>
     </div>
 
